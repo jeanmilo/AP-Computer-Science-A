70326c5a6f2852bbdcdc32eac4c9895c83d68130
Claudia Yamamoto <claudia.parlee@gmail.com>
2018-01-20 14:45:10 -0500

    Create StudentInfoCard.java

---
+++ Mod. 1- Hello Java/StudentInfoCard.java
@@ -0,0 +1,28 @@
+/**
+ * Student Info Card
+ *
+ * @Claudia Yamamoto
+ * Date: Nov. 26
+ * Purpose: A program to print my student information. 
+**/
+public class StudentInfoCard
+{
+    public static void main(String args[]){
+        System.out.println("Name: Claudia Yamamoto, "+"DOB: May 31st, 2000");
+        System.out.println("Age: 17, "+"Grade: 12");
+        System.out.println("");
+        System.out.println("City: Boca Raton");
+        System.out.print("Cell: 754-367-1759 ");
+        System.out.print("Email: claudia.parlee@gmail.com, ");
+        System.out.println("Available: From Monday-Friday, 3-8pm. ");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Current Math: Pre-calculus and AP Statistics");
+        System.out.println("Programming Experience: I learned Javascript while learning how to create my own website,");
+        System.out.println("and I learned Python at a summer immersion program for girls interested in computer science.");
+        System.out.println("");
+        System.out.println("I am taking AP Computer Science: A online because I'm interested in computer science and want to "); 
+        System.out.println("expand my programming knowledge. I also want to major in Computer Science and this course ");
+        System.out.println("would help me show colleges how serious I am about learning about computer science.");
+    }
+}
